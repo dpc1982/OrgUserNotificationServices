11665846fa7dd68790d1f95c83a29a3960b923df
dpc1982 <91042477+dpc1982@users.noreply.github.com>
2026-08-25 11:32:41 +0530

    Department Changes

--- src/main/java/com/catalogue/verg/core/util/VergProperties.java
+++ src/main/java/com/catalogue/verg/core/util/VergProperties.java
@@ -22,13 +22,12 @@ public class VergProperties {
     
         @Value("${elastic.required.field.org.json.path}")
         private String elasticOrgJsonPath;
-    
-        @Value("${elastic.required.field.department.json.path}")
-        private String elasticDepartmentJsonPath;
-    
         @Value("${elastic.required.field.catalogue.json.path}")
         private String elasticCatalogueJsonPath;
     
         @Value("${elastic.required.field.catalogueassignment.json.path}")
         private String elasticCatalogueassignmentJsonPath;
+    
+        @Value("${elastic.required.field.departmenttype.json.path}")
+        private String elasticDepartmenttypeJsonPath;
     }
